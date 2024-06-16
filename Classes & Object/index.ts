deaa771ec51a055fa27ccc762b320e0b144256cb
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-06-16 14:13:57 -0700

    Update index.ts

--- Classes & Object/index.ts
+++ Classes & Object/index.ts
@@ -90,3 +90,56 @@ let car3 = new Car("BMW", "3000CC", 6, false, 3000000);
 console.log(car3);
 
 //                        XXXXXXXXXXXXXXXXXXXXX
+
+// Question # 4
+
+class Car{
+    brand:string;
+    year:number;
+    color:string
+    constructor(brand:string,year:number,color:string){
+        this.brand=brand;
+        this.year=year;
+        this.color=color
+    }
+    start(){
+        return("Car Start!");
+    }
+    stop(){
+        return("Car Stop!");
+    }
+}
+let lexus=new Car("Lexus",2022,"Blue");
+console.log(lexus.color);
+let fortuner=new Car("fortuner",2024,"Black");
+console.log(fortuner);
+let audi=new Car("Audi",2020,"White");
+console.log(audi.brand);
+let corolla=new Car("BMW",2023,"Gray");
+console.log(corolla.stop());
+
+//                        XXXXXXXXXXXXXXXXXXXXX
+
+// Question # 5
+
+class Person{
+    name:string;
+    height:number;
+    email:string;
+    constructor(name:string,height:number,email:string){
+        this.name=name;
+        this.height=height;
+        this.email=email;
+    }
+    work(){
+        return `Solve Problems Solving, Buid Something!`
+    }
+}
+let p1=new Person("Tayyaba",5.2,"tayyabaramzan.pak@gmail.com");
+console.log(p1.work());
+let p2=new Person("Rida",5.4,"ridafatima@gmail.com");
+console.log(p2.name);
+let p3=new Person("Hassan",5.3,"mhassan312@gmail.com");
+console.log(p3.email);
+
+//                        XXXXXXXXXXXXXXXXXXXXX
